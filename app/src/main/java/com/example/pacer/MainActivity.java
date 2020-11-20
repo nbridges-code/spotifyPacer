defb06eaa5e910ee192765908246306f16e26fdf
Nick <nb1115@wildcats.unh.edu>
2020-11-20 11:49:00 -0500

    Changed XML layout

--- app/src/main/java/com/example/pacer/MainActivity.java
+++ app/src/main/java/com/example/pacer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.RemoteControlClient;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
